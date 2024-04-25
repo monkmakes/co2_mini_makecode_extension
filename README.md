0d12ef69748ff641300760cebc2f335798f683c6
simonmonk <srmonk@gmail.com>
2024-04-25 18:23:42 +0100

    first

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# co2_mini_makecode_extension
+This extension makes it easy to use the [MonkMakes CO2 MINI](https://monkmakes.com/co2_mini)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* enter the following URL in the search field: https://github.com/monkmakes/co2_mini_makecode_extension
+
+## Connecting Your micro:bit
+
+Your micro:bit uses pins 0 and 1 to communicate with the CO2 Mini's serial interface. Use alligator clip leads to connect your micro:bit and CO2 Mini as shown below.
+
+![Connecting](https://github.com/monkmakes/plant-monitor-makecode/raw/master/connecting.png)
+
+
+
+## Example Project
+This example shows the CO2 using the bar graph block. Pressing button A will show the temperature in degrees C and pressing button B, the relative humidity as a percentage.
+
+![A rendered view of the blocks](https://github.com/monkmakes/plant-monitor-makecode/raw/master/.github/makecode/blocks.png)
+
+## Documentation
+
+A datasheet and full instructions for the CO2 Mini can be found on the [product page for the CO2 Mini Kit](https://monkmakes.com/co2_mini)
+
---
+++ co2_mini.ts
@@ -0,0 +1,99 @@
+/**
+ * makecode MonkMakes CO2 Mini sensor
+ * MonkMakes Ltd
+ * Author: Simon Monk @ https://www.monkmakes.com
+ * Date: 2024-04-25
+ */
+
+
+/**
+ * MonkMakes CO2 Mini block
+ */
+//% color=190 weight=100 icon="\uf1bb" block="CO2 Mini"
+namespace CO2Mini {
+
+    let value_str = ""
+    let co2 = -1
+    let temp = -1
+    let humidity = -1
+
+    serial.onDataReceived(serial.delimiters(Delimiters.NewLine), () => {
+        let response = serial.readUntil(serial.delimiters(Delimiters.NewLine))
+        //basic.showString(response)
+        value_str = response.substr(2, response.length-3) // w=123\r\n
+        //basic.showString(value_str)
+        let value = parseInt(value_str)
+        //basic.showString(response.charAt(0))
+        if (response.charAt(0) == 'c') {
+            co2 = value
+        }
+        if (response.charAt(0) == 't') {
+            temp = value
+        }
+        if (response.charAt(0) == 'h') {
+            humidity = value
+        }
+    })
+
+
+    /**
+     * Start the CO2 Mini - this will redirect serial to P1 and P2
+     */
+    //% blockId=device_init block="start plant monitor"
+    export function startMon(): void {
+        serial.redirect(
+            SerialPin.P2,
+            SerialPin.P1,
+            BaudRate.BaudRate9600
+        )
+    }
+
+    /**
+    * Return the CO2 parts per million.
+    */
+    //% blockId=device_wetness block="plant wetness"
+    export function readCO2(): number {
+        serial.writeString("c")
+        basic.pause(200)
+        return co2
+    }
+
+
+    /**
+    * Return the temperature in degrees C.
+    */
+    //% blockId=device_temp block="plant temperature"
+    export function readTemp(): number {
+        serial.writeString("t")
+        basic.pause(200)
+        return temp
+    }
+
+
+    /**
+    * Return the relative humidity as a percentage.
+    */
+    //% blockId=device_humidity block="plant humidity"
+    export function readHumidity(): number {
+        serial.writeString("h")
+        basic.pause(200)
+        return humidity
+    }
+
+    /**
+    * Turn the LED on
+    */
+    //% blockId=device_led_on block="plant monitor LED on"
+    export function monitor_led_on(): void {
+        serial.writeString("L")
+    }
+
+    /**
+    * Turn the LED off
+    */
+    //% blockId=device_led_off block="plant monitor LED off"
+    export function monitor_led_off(): void {
+        serial.writeString("l")
+    }
+
+}
